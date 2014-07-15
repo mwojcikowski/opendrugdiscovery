66f5d33b021ce89f6fb329a5a0aaf76196be42fd
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-07-15 10:21:36 +0200

    Make angle function work on multiple arguments

--- interactions.py
+++ interactions.py
@@ -487,15 +487,17 @@ def halogenbond(mol1, mol2):
 
 def angle(v1, v2):
     """ Return an angle between two vectors in degrees """
-    v1 = np.array(v1).flatten()
-    v2 = np.array(v2).flatten()
-    return np.nan_to_num(np.degrees(np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1)* np.linalg.norm(v2)))))
+    v1 = np.array(v1)
+    v2 = np.array(v2)
+    dot = (v1*v2).sum(axis=-1) # better than np.dot(v1, v2), multiple vectors can be applied
+    norm = np.linalg.norm(v1, axis=-1)* np.linalg.norm(v2, axis=-1)
+    return np.nan_to_num(np.degrees(np.arccos(dot/norm)))
     
-def dihedral(v1,v2,v3,v4):
+def dihedral(p1,p2,p3,p4):
     """ Calculate dihedral from 4 points """
-    v12 = (v1-v2)/np.linalg.norm(v1-v2)
-    v23 = (v2-v3)/np.linalg.norm(v2-v3)
-    v34 = (v3-v4)/np.linalg.norm(v3-v4)
+    v12 = (p1-p2)/np.linalg.norm(p1-p2)
+    v23 = (p2-p3)/np.linalg.norm(p2-p3)
+    v34 = (p3-p4)/np.linalg.norm(p3-p4)
     return angle(np.cross(v12, v23), np.cross(v23, v34))
 
 
