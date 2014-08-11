cfa77bed245d80d453f7b463ea35473bbc29cc50
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-11 14:27:16 +0200

    Patch readfile not to return mols

--- toolkits/rdk.py
+++ toolkits/rdk.py
@@ -8,3 +8,11 @@ class Fingerprint(rdk.Fingerprint):
         return np.array(self.fp, dtype=bool)
         
 rdk.Fingerprint = Fingerprint
+
+# Patch reader not to return None as molecules
+def _readfile(format, filename):
+    for mol in rdk.readfile(format, filename):
+        if mol is not None:
+            yield mol
+
+rdk.readfile = _readfile
