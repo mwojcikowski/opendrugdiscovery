9410981ee5fb572a1d6ee17154b57917febb272d
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-18 13:24:21 +0200

    RFscore #1

--- scoring/functions/RFScore.py
+++ scoring/functions/RFScore.py
@@ -1,57 +1,81 @@
 import csv
 import numpy as np
-from machinelearning.randomforest import randomforest
-from descriptors import close_contact, Molecule
-import pybel
+from multiprocessing import Pool
 
-ligand_atomic_num = [6,7,8,9,15,16,17,35,53]
-protein_atomic_num = [6,7,8,16]
+from oddt.toolkits import ob as toolkit
+from oddt.scoring import scorer
+from oddt.scoring.machinelearning.randomforest import randomforest
+from oddt.scoring.descriptors import close_contacts
 
-# 12A cutoff
+# RF-Score settings
+ligand_atomic_nums = [6,7,8,9,15,16,17,35,53]
+protein_atomic_nums = [6,7,8,16]
 cutoff = 12
 
-# Supress Pybel warnings
-pybel.ob.obErrorLog.StopLogging()
+# define sub-function for paralelization
+def generate_descriptor(packed):
+    pdbid, gen, pdbbind_dir, pdbbind_version = packed
+    protein_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_protein.pdb" % (pdbid, pdbid)
+    ligand_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_ligand.sdf" % (pdbid, pdbid)
+    protein = toolkit.readfile("pdb", protein_file, opt = {'b': None}).next()
+    ligand = toolkit.readfile("sdf", ligand_file).next()
+    return gen.build([ligand], protein).flatten()
 
-def score(protein_file, ligand_file, protein_file_type='pdb', ligand_file_type='sdf', descriptors_file='RF-Score_descriptors.csv'):
-    """ Calculate scoring function using ligands and protein """
-    # build forest
-    rf = randomforest()
+ # skip comments and merge multiple spaces
+def _csv_file_filter(f):
+    for row in open(f, 'rb'):
+        if row[0] == '#':
+            continue
+        yield ' '.join(row.split())
+
+class rfscore(scorer):
+    def __init__(self, protein = None, n_jobs = -1, **kwargs):
+        self.protein = protein
+        self.n_jobs = n_jobs
+        model = randomforest(n_estimators = 500, oob_score = True, n_jobs = n_jobs, **kwargs)
+        descriptors = close_contacts(protein, cutoff = cutoff, protein_types = protein_atomic_nums, ligand_types = ligand_atomic_nums)
+        super(rfscore,self).__init__(model, descriptors)
     
-    # train forest on prebuilt descriptors from PDBind 2007
-    train_data = np.loadtxt(descriptors_file, delimiter=',')
-    rf.train(train_data[:,1:],train_data[:,0])
-    
-    # read files
-    protein = Molecule(pybel.readfile(protein_file_type, protein_file, opt = {'b': None}).next())
-    descs = np.zeros((1,36))
-    # build descriptors for ligand
-    for lig in pybel.readfile(ligand_file_type, ligand_file):
-        ligand = Molecule(lig)
-        #print lig.title
-        desc = close_contact(protein.coordinate_dict(protein.atom_dict_atomicnum(protein_atomic_num)), ligand.coordinate_dict(ligand.atom_dict_atomicnum(ligand_atomic_num)), cutoff).flatten()
-        descs = np.vstack((descs, desc))
-    
-    # build descriptors and score molecules
-    print rf.predict(descs[1:])
-    
-def prepare():
-    """ Prepare base descriptors to train generic RF-Score """
-    train_set = np.zeros((1,37)) # 32 descriptors and #33 activity
-    for pdbid, act in csv.reader(open('functions/RFScore/PDBbind_core07.txt', 'rb'), delimiter='\t'):
+    def train(self, pdbbind_dir, pdbbind_version = '2013', sf_pickle = ''):
+        core_desc = np.zeros((1,36), dtype=int)
+        core_act = np.zeros(1, dtype=float)
+        refined_desc = np.zeros((1,36), dtype=int)
+        refined_act = np.zeros(1, dtype=float)
         
-        protein_file = "functions/RFScore/v2007/%s/%s_protein.pdb" % (pdbid, pdbid)
-        ligand_file = "functions/RFScore/v2007/%s/%s_ligand.sdf" % (pdbid, pdbid)
+        # build train and test 
+        cpus = self.n_jobs if self.n_jobs > 0 else None
+        pool = Pool(processes=cpus)
         
-        protein = Molecule(pybel.readfile("pdb", protein_file, opt = {'b': None}).next())
-        ligand = Molecule(pybel.readfile("sdf", ligand_file).next())
+        core_set = []
+        for row in csv.reader(_csv_file_filter(pdbbind_dir + "/v" + pdbbind_version + "/INDEX_core_data." + pdbbind_version), delimiter=' '):
+            pdbid = row[0]
+            act = float(row[3])
+            core_set.append(pdbid)
+            core_act = np.vstack((core_act, act))
         
-        #print protein.coordinate_dict(ligand_atomic_num)
-        #print np.nonzero(protein.coordinate_dict(ligand_atomic_num))
-        desc = close_contact(protein.coordinate_dict(protein_atomic_num), ligand.coordinate_dict(ligand_atomic_num), cutoff).flatten()
-        train_set = np.vstack((train_set, np.append(act, desc)))
-    np.savetxt('RF-Score_descriptors.csv', train_set[1:], delimiter=',', fmt='%s')
-    print train_set[1:,1:]
-    
-#def validate():
+        result = pool.map(generate_descriptor, [(pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version) for pdbid in core_set])
+        core_desc = np.vstack(result)
+        core_act = core_act[1:]
+        
+        refined_set = []
+        for row in csv.reader(_csv_file_filter(pdbbind_dir + "/v" + pdbbind_version + "/INDEX_refined_data." + pdbbind_version), delimiter=' '):
+            pdbid = row[0]
+            act = float(row[3])
+            if pdbid in core_set:
+                continue
+            refined_set.append(pdbid)
+            refined_act = np.vstack((refined_act, act))
+        
+        result = pool.map(generate_descriptor, [(pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version) for pdbid in refined_set])
+        refined_desc = np.vstack(result)
+        refined_act = refined_act[1:]
+        
+        np.savetxt('RF-Score_descriptors.csv', np.hstack((core_act, core_desc)), delimiter=',', fmt='%s')
+        print refined_act.dtype, refined_desc.dtype
+        self.model.fit(refined_desc, refined_act.flatten())
+        rmse = np.sqrt(np.mean(np.square(self.model.oob_prediction_ - refined_act.flatten())))
+        print 'Train set:', self.model.score(core_desc, core_act.flatten()), 'RMSE:', rmse
+        print 'Test set:', self.model.score(refined_desc, refined_act.flatten())
+        if sf_pickle:
+            self.save(sf_pickle)
 
