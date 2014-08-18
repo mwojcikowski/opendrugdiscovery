3cf376aec6d3c14003d29da2d330333609071f17
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-18 16:56:39 +0200

    Simply pass RF regressor

--- scoring/machinelearning/randomforest.py
+++ scoring/machinelearning/randomforest.py
@@ -1,11 +1,1 @@
-from sklearn.ensemble import RandomForestRegressor
-
-class randomforest(RandomForestRegressor):
-    def __init__(self, n_trees = 500, n_jobs = -1):
-        self.model = RandomForestRegressor(n_estimators = n_trees, oob_score = True, random_state=1, n_jobs=n_jobs)
-    
-    def fit(self, input_descriptors, target_values):
-        self.model.fit(input_descriptors, target_values)
-    
-    def predict(self, input_descriptors):
-         return self.model.predict(input_descriptors)
+from sklearn.ensemble import RandomForestRegressor as randomforest
