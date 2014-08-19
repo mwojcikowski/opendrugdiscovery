441dbecbedfa9487bb82e4ad8988b0491d45d25e
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-19 15:28:05 +0200

    Ensemble scorer #1

--- scoring/__init__.py
+++ scoring/__init__.py
@@ -10,7 +10,7 @@ class scorer(object):
     def fit(self, ligands, target):
         self.train_descs = self.descriptor_generator.build(ligands)
         self.train_target = target
-        return self.model.fit(descs,target)
+        return self.model.fit(self.train_descs,target)
     
     def predict(self, ligands):
         descs = self.descriptor_generator.build(ligands)
@@ -20,6 +20,75 @@ class scorer(object):
         descs = self.descriptor_generator.build(ligands)
         return self.model.score(descs,target)
     
+    def cross_validate(n = 10, test_set = None, test_target = None):
+        if test_set and test_target:
+            cv_set = np.vstack((self.train_descs, test_set))
+            cv_target = np.vstack((self.train_target, test_target))
+        else:
+            cv_set = self.train_descs
+            cv_target = self.train_target
+        return cross_val_score(self.model, cv_set, cv_target, cv = n)
+        
+    def save(self, filename):
+        return pickle.dump(self, filename, compress=1)
+    
+    @classmethod
+    def load(self, filename):
+        return pickle.load(filename)
+
+
+### FIX ### If possible make ensemble scorer lazy, for now it consumes all ligands
+class ensemble_scorer(object):
+    def __init__(self, model_instances, descriptor_generator_instances):
+        self.models = model_instances
+        if type(descriptor_generator_instances) is list:
+            self.single_model = False
+        else:
+            self.single_model = True
+        
+        self.descriptor_generators = descriptor_generator_instances
+        if type(descriptor_generator_instances) is list:
+            if len(descriptor_generator_instances) == len(model_instances):
+                raise ValueError, "Length of models list doesn't equal descriptors list"
+            self.single_descriptor = False
+        else:
+            self.single_descriptor = True
+        
+    def fit(self, ligands, target):
+        if self.single_descriptor:
+            self.train_descs = self.descriptor_generators.build(ligands)
+        else:
+            self.train_descs = [desc_gen.build(ligands) for desc_gen in self.descriptor_generators]
+        self.train_target = target
+        
+        if self.single_model and self.single_descriptor:
+            return model.fit(self.train_descs,target)
+        elif self.single_model and not self.single_descriptor:
+            return [model.fit(desc,target) for desc in self.train_descs]
+        else:
+            return [model.fit(self.train_descs[n],target) for n, model in enumerate(self.models)]
+    
+    def predict(self, ligands):
+        if self.single_model and self.single_descriptor:
+            descs = self.descriptor_generator.build(ligands)
+            return self.models.predict(descs)
+        elif self.single_model and not self.single_descriptor:
+            return [self.models.predict(descs) for desc in self.train_descs]
+        else:
+            descs = [desc_gen.build(ligands) for desc_gen in self.descriptor_generators]
+            return [model.predict(descs[n],target) for n, model in enumerate(self.models)]
+    
+    def score(self, ligands, target):
+        if self.single_model and self.single_descriptor:
+            descs = self.descriptor_generator.build(ligands)
+            return self.models.score(descs)
+        elif self.single_model and not self.single_descriptor:
+            return [self.models.score(descs) for desc in self.train_descs]
+        else:
+            descs = [desc_gen.build(ligands) for desc_gen in self.descriptor_generators]
+            return [model.score(descs[n],target) for n, model in enumerate(self.models)]
+    
+    ### TODO ### Test
     def cross_validate(n = 10, test_set = None, test_target = None):
         if test_set and test_target:
             cv_set = np.vstack((self.train_descs, test_set))
