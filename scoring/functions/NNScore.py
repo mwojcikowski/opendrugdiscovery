5cba406bc2c5c6a6f8e46ba010f14d022de2e829
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-20 16:27:04 +0200

    NNScore #115

--- scoring/functions/NNScore.py
+++ scoring/functions/NNScore.py
@@ -1,5 +1,5 @@
 import csv
-from os.path import dirname
+from os.path import dirname, isfile
 import numpy as np
 from multiprocessing import Pool
 import warnings
@@ -15,7 +15,9 @@ warnings.simplefilter("ignore", RuntimeWarning)
 # define sub-function for paralelization
 def generate_descriptor(packed):
     pdbid, gen, pdbbind_dir, pdbbind_version = packed
-    protein_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_protein.pdb" % (pdbid, pdbid)
+    protein_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_pocket.pdb" % (pdbid, pdbid)
+    if not isfile(protein_file):
+        protein_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_protein.pdb" % (pdbid, pdbid)
     ligand_file = pdbbind_dir + "/v" + pdbbind_version + "/%s/%s_ligand.sdf" % (pdbid, pdbid)
     protein = toolkit.readfile("pdb", protein_file).next()
     # mark it as a protein
@@ -43,23 +45,55 @@ class nnscore(scorer):
         cpus = self.n_jobs if self.n_jobs > 0 else None
         pool = Pool(processes=cpus)
         
+        core_act = np.zeros(1, dtype=float)
+        core_set = []
+        if pdbbind_version == '2007':
+            csv_file = pdbbind_dir + "/v" + pdbbind_version + "/INDEX." + pdbbind_version + ".core.data"
+        else:
+            csv_file = pdbbind_dir + "/v" + pdbbind_version + "/INDEX_core_data." + pdbbind_version
+        for row in csv.reader(_csv_file_filter(csv_file), delimiter=' '):
+            pdbid = row[0]
+            act = float(row[3])
+            core_set.append(pdbid)
+            core_act = np.vstack((core_act, act))
+        
+        result = pool.map(generate_descriptor, [(pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version) for pdbid in core_set])
+        core_desc = np.vstack(result)
+        core_act = core_act[1:]
+        
         refined_act = np.zeros(1, dtype=float)
         refined_set = []
         if pdbbind_version == '2007':
-            csv_file = pdbbind_dir + "/v" + pdbbind_version + "/INDEX." + pdbbind_version + ".core.data"
+            csv_file = pdbbind_dir + "/v" + pdbbind_version + "/INDEX." + pdbbind_version + ".refined.data"
         else:
             csv_file = pdbbind_dir + "/v" + pdbbind_version + "/INDEX_refined_data." + pdbbind_version
         for row in csv.reader(_csv_file_filter(csv_file), delimiter=' '):
             pdbid = row[0]
             act = float(row[3])
+            if pdbid in core_set:
+                continue
             refined_set.append(pdbid)
             refined_act = np.vstack((refined_act, act))
         
+        #result = [generate_descriptor((pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version)) for pdbid in refined_set]
         result = pool.map(generate_descriptor, [(pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version) for pdbid in refined_set])
-        #result = [generate_descriptor(a) for a in [(pdbid, self.descriptor_generator, pdbbind_dir, pdbbind_version) for pdbid in refined_set]]
         refined_desc = np.vstack(result)
         refined_act = refined_act[1:]
         
         self.train_descs = refined_desc
         self.train_target = refined_act
         
+        self.test_descs = core_desc
+        self.test_target = core_act
+        
+        if sf_pickle:
+            self.save(sf_pickle)
+        else:
+            self.save(dirname(__file__) + '/NNscore.pickle')
+        
+    @classmethod
+    def load(self, filename = ''):
+        if not filename:
+            filename = dirname(__file__) + '/NNscore.pickle'
+        return scorer.load(filename)
+
