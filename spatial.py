8fef0249f9cfbadb063e66088b20a6c56cb5ff6b
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-25 14:46:57 +0200

    Add documentation

--- spatial.py
+++ spatial.py
@@ -1,21 +1,61 @@
+"""Spatial functions included in ODDT
+Mainly used by other modules, but can be accessed directly.
+"""
+
 import numpy as np
 from scipy.spatial.distance import cdist as distance
 
+__all__ = ['angle', 'angle_2v', 'dihedral', 'distance']
+
 # angle functions
 def angle(p1,p2,p3):
-    """ Return an angle from 3 points in cartesian space (point #2 is centroid) """
+    """Returns an angle from a series of 3 points (point #2 is centroid).Angle is returned in degrees.
+    
+    Parameters
+    ----------
+    p1,p2,p3 : numpy arrays, shape = [n_points, n_dimensions]
+        Triplets of points in n-dimensional space, aligned in rows.
+    
+    Returns
+    -------
+    angles : numpy array, shape = [n_points]
+        Series of angles in degrees
+    """
     v1 = p1-p2
     v2 = p3-p2
     return angle_2v(v1,v2)
 
 def angle_2v(v1, v2):
-    """ Return an angle between two vectors in degrees """
+    """Returns an angle from a series of 3 points (point #2 is centroid).Angle is returned in degrees.
+    
+    Parameters
+    ----------
+    v1,v2 : numpy arrays, shape = [n_vectors, n_dimensions]
+        Pairs of vectors in n-dimensional space, aligned in rows.
+    
+    Returns
+    -------
+    angles : numpy array, shape = [n_vectors]
+        Series of angles in degrees
+    """
     dot = (v1*v2).sum(axis=-1) # better than np.dot(v1, v2), multiple vectors can be applied
     norm = np.linalg.norm(v1, axis=-1)* np.linalg.norm(v2, axis=-1)
     return np.degrees(np.arccos(dot/norm))
 
 def dihedral(p1,p2,p3,p4):
-    """ Calculate dihedral from 4 points """
+    """Returns an dihedral angle from a series of 4 points. Dihedral is returned in degrees.
+    Function distingishes clockwise and antyclockwise dihedrals.
+    
+    Parameters
+    ----------
+    p1,p2,p3,p4 : numpy arrays, shape = [n_points, n_dimensions]
+        Triplets of points in n-dimensional space, aligned in rows.
+    
+    Returns
+    -------
+    angles : numpy array, shape = [n_points]
+        Series of angles in degrees
+    """
     v12 = (p1-p2)/np.linalg.norm(p1-p2)
     v23 = (p2-p3)/np.linalg.norm(p2-p3)
     v34 = (p3-p4)/np.linalg.norm(p3-p4)
