f6841a527bd8b52aa88c4c3b5980a0001387f33e
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-26 16:14:00 +0200

    Make models inherit from sklearn

--- scoring/models/regressors.py
+++ scoring/models/regressors.py
@@ -1,7 +1,16 @@
-from sklearn.ensemble import RandomForestRegressor as randomforest
-from sklearn.svm import SVR as svm
-from sklearn.pls import PLSRegression as pls
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.pls import PLSRegression
 
 from .neuralnetwork import neuralnetwork
 
 __all__ = ['randomforest', 'svm', 'pls', 'neuralnetwork']
+
+class randomforest(RandomForestRegressor):
+    pass
+
+class svm(SVR):
+    pass
+
+class svm(PLSRegression):
+    pass
