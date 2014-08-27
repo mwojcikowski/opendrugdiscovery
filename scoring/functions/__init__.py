c0336dba222f6e577f989d7526661e3979351a40
Maciek Wójcikowski <maciek@wojcikowski.pl>
2014-08-27 16:26:24 +0200

    Add SF list. Add nn get_params and set params methods

---
+++ scoring/functions/__init__.py
@@ -0,0 +1,4 @@
+from .RFScore import rfscore
+from .NNscore import nnscore
+
+__all__ = ['rfscore', 'nnscore']
--- scoring/models/neuralnetwork.py
+++ scoring/models/neuralnetwork.py
@@ -4,14 +4,16 @@ import numpy as np
 from scipy.stats import linregress
 
 class neuralnetwork:
-    def __init__(self, shape, loadnet=None, full_conn=True, biases=True, random_weights = True):
+    def __init__(self, shape = None, full_conn=True, biases=True, random_weights = True):
         """
         shape: shape of a NN given as a tuple
         """
-        if loadnet:
-            self.model = ffnet()
-            self.model.load(loadnet)
-        else:
+        self.shape = shape
+        self.full_conn = full_conn
+        self.biases = biases
+        self.random_weights = random_weights
+        
+        if shape:
             if full_conn:
                 conec = tmlgraph(shape, biases)
             else:
@@ -20,8 +22,16 @@ class neuralnetwork:
             if random_weights:
                 self.model.randomweights()
     
+    def get_params(self, deep=True):
+        return {'shape': self.shape, 'full_conn': self.full_conn, 'biases': self.biases, 'random_weights': self.random_weights}
+    
+    def set_params(self, args):
+        self.__init__(**args)
+        return self
+    
     def fit(self, input_descriptors, target_values, train_alg='tnc', **kwargs):
         getattr(self.model, 'train_'+train_alg)(input_descriptors, target_values, **kwargs)
+        return self
     
     def predict(self, input_descriptors):
         return np.array(self.model.call(input_descriptors))
