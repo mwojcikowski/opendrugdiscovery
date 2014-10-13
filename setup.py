10de483e4d1decf6099e52c491fa0347215714c2
maciek <maciek@debian-laptop>
2014-10-13 11:46:44 +0200

    Move ODDT module to separate directory

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='ODDT',
                     'oddt.scoring.descriptors',
                     'oddt.docking',
                     ],
-    package_dir={'oddt': ''}, 
+#    package_dir={'oddt': ''}, 
     package_data={'oddt.scoring.functions': ['NNScore/*.csv', 'RFScore/*.csv']},
     )
